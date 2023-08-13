e3c4da6243d18ca9fda6afd44a1de13887440af5
nbogdankov@gmail.com <nbogdankov@gmail.com>
2023-08-13 23:52:47 +0300

    some fixes

--- src/Components/MyWorks/MyWorks.jsx
+++ src/Components/MyWorks/MyWorks.jsx
@@ -15,101 +15,16 @@ import ittc4 from "../../images/ittc4.png";
 import om1 from "../../images/om1.png";
 import om2 from "../../images/om2.png";
 import om3 from "../../images/om3.png";
-import jc1 from '../../images/jc1.png';
-import jc2 from '../../images/jc2.png';
-import jc3 from '../../images/jc3.png';
-import jc4 from '../../images/jc4.png';
+import jc1 from "../../images/jc1.png";
+import jc2 from "../../images/jc2.png";
+import jc3 from "../../images/jc3.png";
+import jc4 from "../../images/jc4.png";
 
 const MyWorks = () => {
 	return (
 		<div className={styles.works}>
 			<div className={styles.title}>My Works</div>
 			<div className={styles.blocks}>
-				<div className={cn(styles.block, styles.first)}>
-					<div className={styles.left}>
-						<div className={styles.left_info}>
-							<div className={styles.left_info_title}>
-								Friendly Carrier
-							</div>
-							<div className={styles.left_info_text}>
-								I worked in this startup as the{" "}
-								<span>designer </span>. I designed{" "}
-								<span>UI/UX</span> for all the pages. I was in
-								this project from the beginning. Also, I{" "}
-								<span>adapted</span> all the pages{" "}
-								<span>for mobile devices</span> in code.
-							</div>
-						</div>
-						<div className={styles.left_links}>
-							<a href="#">Site</a>, <a href="#">Figma</a>
-						</div>
-					</div>
-					<div className={styles.right}>
-						<div className={styles.right_block}>
-							<div className={styles.right_column}>
-								<img
-									className={styles.right_column_image}
-									src={fc1}
-									alt=""
-								/>
-								<img
-									className={styles.right_column_image}
-									src={fc2}
-									alt=""
-								/>
-							</div>
-							<div className={styles.right_column}>
-								<img
-									className={styles.right_column_image}
-									src={fc3}
-									alt=""
-								/>
-								<img
-									className={styles.right_column_image}
-									src={fc4}
-									alt=""
-								/>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className={cn(styles.block, styles.second)}>
-					<div className={styles.left}>
-						<div className={styles.left_info}>
-							<div className={styles.left_info_title}>
-								Try this
-							</div>
-							<div className={styles.left_info_text}>
-								I worked in this startup as an{" "}
-								<span> outsource designer</span>. It is a mobile
-								app which is made for trying new clothes. I made{" "}
-								<span>UX/UI</span> designs for this project.
-							</div>
-						</div>
-						<div className={styles.left_links}>
-							<a href="#">Figma</a>
-						</div>
-					</div>
-					<div className={styles.right_another}>
-						<div className={styles.right_another_block}>
-							<img
-								className={styles.right_another_image}
-								src={tt1}
-								alt=""
-							/>
-							<img
-								className={styles.right_another_image}
-								src={tt2}
-								alt=""
-							/>
-							<img
-								className={styles.right_another_image}
-								src={tt3}
-								alt=""
-							/>
-						</div>
-					</div>
-				</div>
 				<div className={cn(styles.block, styles.first)}>
 					<div className={styles.left}>
 						<div className={styles.left_info}>
@@ -126,7 +41,13 @@ const MyWorks = () => {
 							</div>
 						</div>
 						<div className={styles.left_links}>
-							<a href="#">Site(old)</a>, <a href="#">Figma</a>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://www.figma.com/file/xIRYM7BFcV15q4hXwSEfop/TableTennis?type=design&node-id=388%3A619&mode=design&t=v2IOngT9DGJHPSVy-1"
+							>
+								Figma
+							</a>
 						</div>
 					</div>
 					<div className={styles.right}>
@@ -162,35 +83,40 @@ const MyWorks = () => {
 					<div className={styles.left}>
 						<div className={styles.left_info}>
 							<div className={styles.left_info_title}>
-								OneMenu
+								Try this
 							</div>
 							<div className={styles.left_info_text}>
-								There was an idea to unify all menus in one. I
-								<span>made UI/UX design and frontend</span>.
-								This WebApp uses users’ colors. You can check
-								how WebApp looks on different devices with{" "}
-								<span>different preset user colors</span>.
+								I worked in this startup as an{" "}
+								<span> outsource designer</span>. It is a mobile
+								app which is made for trying new clothes. I made{" "}
+								<span>UX/UI</span> designs for this project.
 							</div>
 						</div>
 						<div className={styles.left_links}>
-							<a href="#">Figma</a>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://www.figma.com/file/fd251qHK8AnIhVUxTA5AxU/try-this?type=design&node-id=1%3A515&mode=design&t=bQKPI8AKxzeYX3Ar-1"
+							>
+								Figma
+							</a>
 						</div>
 					</div>
 					<div className={styles.right_another}>
 						<div className={styles.right_another_block}>
 							<img
 								className={styles.right_another_image}
-								src={om1}
+								src={tt1}
 								alt=""
 							/>
 							<img
 								className={styles.right_another_image}
-								src={om2}
+								src={tt2}
 								alt=""
 							/>
 							<img
 								className={styles.right_another_image}
-								src={om3}
+								src={tt3}
 								alt=""
 							/>
 						</div>
@@ -217,7 +143,21 @@ const MyWorks = () => {
 							</div>
 						</div>
 						<div className={styles.left_links}>
-							<a href="#">Site</a>, <a href="#">Figma</a>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://jellycraft.net/"
+							>
+								Site
+							</a>
+							,{" "}
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://www.figma.com/file/Wux0QQ44VwgCWhLI11mLNA/something?type=design&node-id=0%3A1&mode=design&t=h0lRtwvnN1yf9EHU-1"
+							>
+								Figma
+							</a>
 						</div>
 					</div>
 					<div className={styles.right}>
@@ -243,6 +183,112 @@ const MyWorks = () => {
 								<img
 									className={styles.right_column_image}
 									src={jc4}
+									alt=""
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className={cn(styles.block, styles.second)}>
+					<div className={styles.left}>
+						<div className={styles.left_info}>
+							<div className={styles.left_info_title}>
+								OneMenu
+							</div>
+							<div className={styles.left_info_text}>
+								There was an idea to unify all menus in one. I
+								<span>made UI/UX design and frontend</span>.
+								This WebApp uses users’ colors. You can check
+								how WebApp looks on different devices with{" "}
+								<span>different preset user colors</span>.
+							</div>
+						</div>
+						<div className={styles.left_links}>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://www.figma.com/file/m78xN6GSOK6ZTT8gnUGFHx/OneMenu?type=design&node-id=63%3A11&mode=design&t=FnTlKymDxGIn8cd9-1"
+							>
+								Figma
+							</a>
+						</div>
+					</div>
+					<div className={styles.right_another}>
+						<div className={styles.right_another_block}>
+							<img
+								className={styles.right_another_image}
+								src={om1}
+								alt=""
+							/>
+							<img
+								className={styles.right_another_image}
+								src={om2}
+								alt=""
+							/>
+							<img
+								className={styles.right_another_image}
+								src={om3}
+								alt=""
+							/>
+						</div>
+					</div>
+				</div>
+				<div className={cn(styles.block, styles.first)}>
+					<div className={styles.left}>
+						<div className={styles.left_info}>
+							<div className={styles.left_info_title}>
+								Friendly Carrier
+							</div>
+							<div className={styles.left_info_text}>
+								I worked in this startup as the{" "}
+								<span>designer </span>. I designed{" "}
+								<span>UI/UX</span> for all the pages. I was in
+								this project from the beginning. Also, I{" "}
+								<span>adapted</span> all the pages{" "}
+								<span>for mobile devices</span> in code.
+							</div>
+						</div>
+						<div className={styles.left_links}>
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://friendlycarrier.com/"
+							>
+								Site
+							</a>
+							,{" "}
+							<a
+								target="_blank"
+								rel="noreferrer"
+								href="https://www.figma.com/file/V0WaAKDHpUDIsAQQ2gtOKS/MUI-for-Figma-(Community-version)-(with-Material-UI)-(Community)?type=design&node-id=5703%3A69518&mode=design&t=4e1cQ5NOfvm48EgS-1"
+							>
+								Figma
+							</a>
+						</div>
+					</div>
+					<div className={styles.right}>
+						<div className={styles.right_block}>
+							<div className={styles.right_column}>
+								<img
+									className={styles.right_column_image}
+									src={fc1}
+									alt=""
+								/>
+								<img
+									className={styles.right_column_image}
+									src={fc2}
+									alt=""
+								/>
+							</div>
+							<div className={styles.right_column}>
+								<img
+									className={styles.right_column_image}
+									src={fc3}
+									alt=""
+								/>
+								<img
+									className={styles.right_column_image}
+									src={fc4}
 									alt=""
 								/>
 							</div>
