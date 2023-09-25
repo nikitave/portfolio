0a182a8be2bc93acdc5dad7673e70a7b5be1d008
nbogdankov@gmail.com <nbogdankov@gmail.com>
2023-09-25 11:13:38 +0300

    header is adopted for mobile devices

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,43 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
 import { Link, useLocation } from "react-router-dom";
 import cn from "classnames";
+import menu from "../../images/menu.svg";
+import close from "../../images/close.svg";
 
-const Header = ( {color, colorTwo} ) => {
-  const location = useLocation();
+const Header = ({ color, colorTwo }) => {
+    const location = useLocation();
 
-  return (
-    <div className={styles.header}>
-      <div className={styles.links}>
-        <Link
-          className={cn(styles.link, {
-            [styles.active]: location.pathname === "/about-me",
-            [styles.activeD]: colorTwo
-          })}
-          to="/about-me"
-        >
-          About Me
-        </Link>
-        <Link
-          className={cn(styles.link, {
-            [styles.active]: location.pathname === "/my-works",
-          })}
-          to="/my-works"
-        >
-          My Works
-        </Link>
-        <Link
-          className={cn(styles.link, {
-            [styles.active]: location.pathname === "/contact-me",
-            [styles.activeD]: color
-          })}
-          to="/contact-me"
-        >
-          Contact Me
-        </Link>
-      </div>
-    </div>
-  );
+    const [isToggled, setIsToggled] = useState(false);
+
+    const toggle = () => {
+        setIsToggled(!isToggled);
+    };
+
+    return (
+        <div className={styles.header}>
+            <div className={styles.links}>
+                <Link
+                    className={cn(styles.link, {
+                        [styles.active]: location.pathname === "/about-me",
+                        [styles.activeD]: colorTwo,
+                    })}
+                    to="/about-me"
+                >
+                    About Me
+                </Link>
+                <Link
+                    className={cn(styles.link, {
+                        [styles.active]: location.pathname === "/my-works",
+                    })}
+                    to="/my-works"
+                >
+                    My Works
+                </Link>
+                <Link
+                    className={cn(styles.link, {
+                        [styles.active]: location.pathname === "/contact-me",
+                        [styles.activeD]: color,
+                    })}
+                    to="/contact-me"
+                >
+                    Contact Me
+                </Link>
+            </div>
+            <div
+                className={cn(
+                    styles.links_mobile,
+                    isToggled ? styles.active : null
+                )}
+            >
+                <button
+                    className={cn(
+                        styles.burger_button,
+                        styles.burger_button_open
+                    )}
+                    type="button"
+                    onClick={toggle}
+                >
+                    <img className={styles.menu_image} src={close} alt="" />
+                </button>
+                <div className={styles.link_flex}>
+                    <Link
+                        className={cn(styles.mobile_link, {
+                            [styles.mobile_link_active]:
+                                location.pathname === "/about-me",
+                        })}
+                        to="/about-me"
+                    >
+                        About Me
+                    </Link>
+                    <Link
+                        className={cn(styles.mobile_link, {
+                            [styles.mobile_link_active]:
+                                location.pathname === "/my-works",
+                        })}
+                        to="/my-works"
+                    >
+                        My Works
+                    </Link>
+                    <Link
+                        className={cn(styles.mobile_link, {
+                            [styles.mobile_link_active]:
+                                location.pathname === "/contact-me",
+                        })}
+                        to="/contact-me"
+                    >
+                        Contact Me
+                    </Link>
+                </div>
+            </div>
+
+            <div
+                className={cn(
+                    styles.burger,
+                    isToggled ? styles.disappeared : null
+                )}
+            >
+                <button
+                    type="button"
+                    onClick={toggle}
+                    className={styles.burger_button}
+                >
+                    <img className={styles.menu_image} src={menu} alt="" />
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
